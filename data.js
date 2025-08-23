5da858fd5297bb77df7e693798e41ac769e782a8
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-08-23 02:29:13 -0400

    update

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "0-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -37,38 +37,38 @@ var APP_DATA = {
           "yaw": 1.4424583121444652,
           "pitch": -0.4548553170545411,
           "rotation": 0,
-          "target": "1-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "1-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 0.6237626305666808,
           "pitch": 0.06851737476228514,
           "rotation": 0,
-          "target": "2-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "2-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 0.41831118279051793,
           "pitch": -0.28463293989437943,
           "rotation": 0,
-          "target": "3-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "3-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 3.045296875867691,
           "pitch": -0.925936701398296,
           "rotation": 0,
-          "target": "4-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "4-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 0.35967322704928506,
           "pitch": -0.6581772269799266,
           "rotation": 0,
-          "target": "7-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "7-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "1-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -103,44 +103,44 @@ var APP_DATA = {
           "yaw": -1.6714050178449522,
           "pitch": 0.46549617560257417,
           "rotation": 0,
-          "target": "0-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "0-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.005456370557757495,
           "pitch": -0.6945089332476577,
           "rotation": 0,
-          "target": "7-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "7-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 0.0022420591358240927,
           "pitch": -0.20623687189895534,
           "rotation": 0,
-          "target": "3-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "3-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -3.1323887258414835,
           "pitch": -0.5150816921834203,
           "rotation": 0,
-          "target": "4-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "4-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 0.004474134419879761,
           "pitch": 0.4101666489888771,
           "rotation": 0,
-          "target": "2-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "2-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 0.0005245479409374809,
           "pitch": 0.07885594133114182,
           "rotation": 0,
-          "target": "10-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "10-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "2-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -175,44 +175,44 @@ var APP_DATA = {
           "yaw": -2.4657432636741845,
           "pitch": 0.09136221638786779,
           "rotation": 0,
-          "target": "0-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "0-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.026012781872690738,
           "pitch": -0.6067139515170208,
           "rotation": 0,
-          "target": "3-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "3-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -3.1128361947799057,
           "pitch": -0.574402700584864,
           "rotation": 0,
-          "target": "4-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "4-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -3.1175237389903394,
           "pitch": -0.251287640663584,
           "rotation": 0,
-          "target": "1-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "1-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.015547870633257688,
           "pitch": 0.0037740231641532063,
           "rotation": 0,
-          "target": "9-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "9-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.036586666323762174,
           "pitch": -1.044844813263195,
           "rotation": 0,
-          "target": "7-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "7-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "3-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -247,50 +247,50 @@ var APP_DATA = {
           "yaw": 3.1204121389312505,
           "pitch": 1.2834808775211606,
           "rotation": 0,
-          "target": "8-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "8-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.021803437447534435,
           "pitch": -0.7679940464091182,
           "rotation": 0,
-          "target": "7-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "7-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -3.125745696442717,
           "pitch": -0.20286113926689175,
           "rotation": 0,
-          "target": "4-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "4-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 2.737538836709839,
           "pitch": 0.03830532105924789,
           "rotation": 0,
-          "target": "5-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "5-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -2.5524336758097075,
           "pitch": 0.6230867060733942,
           "rotation": 0,
-          "target": "0-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "0-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -3.097525834982129,
           "pitch": 0.6747679372690225,
           "rotation": 0,
-          "target": "1-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "1-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 0.014593447740322674,
           "pitch": 0.8616729838660078,
           "rotation": 0,
-          "target": "10-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "10-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "4-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -325,38 +325,38 @@ var APP_DATA = {
           "yaw": -0.004441129647387854,
           "pitch": -0.05545701110849599,
           "rotation": 0,
-          "target": "7-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "7-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.005899246799870639,
           "pitch": 0.36944931466493536,
           "rotation": 0,
-          "target": "1-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "1-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 2.4080722172328253,
           "pitch": 0.19150870955408905,
           "rotation": 0,
-          "target": "5-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "5-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 1.9243213066380873,
           "pitch": 0.16930958320519984,
           "rotation": 0,
-          "target": "6-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "6-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.2353772040248554,
           "pitch": 0.42393817678718904,
           "rotation": 0,
-          "target": "0-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "0-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "5-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -391,20 +391,20 @@ var APP_DATA = {
           "yaw": 1.6821295258525222,
           "pitch": 0.027545115310459778,
           "rotation": 0,
-          "target": "6-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "6-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 0.09034662443487917,
           "pitch": -0.3945560309861804,
           "rotation": 0,
-          "target": "4-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "4-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "6-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -439,20 +439,20 @@ var APP_DATA = {
           "yaw": -3.128969068546157,
           "pitch": -0.060421319012350594,
           "rotation": 0,
-          "target": "5-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "5-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -1.8227209809252436,
           "pitch": -0.3696550819174682,
           "rotation": 0,
-          "target": "4-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "4-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "7-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -487,32 +487,32 @@ var APP_DATA = {
           "yaw": 0.26157286580803785,
           "pitch": 0.504811662299872,
           "rotation": 0,
-          "target": "0-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "0-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.004441159914772896,
           "pitch": -0.042165140614592644,
           "rotation": 0,
-          "target": "4-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "4-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.4269913040474904,
           "pitch": 0.075552762884044,
           "rotation": 0,
-          "target": "5-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "5-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.798469439210086,
           "pitch": 0.09572717229406535,
           "rotation": 0,
-          "target": "6-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "6-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "8-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "8-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -547,44 +547,44 @@ var APP_DATA = {
           "yaw": 0.6648244919310748,
           "pitch": 0.12345227803802672,
           "rotation": 0,
-          "target": "0-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "0-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 0.015574601080700745,
           "pitch": -0.16188315529669772,
           "rotation": 0,
-          "target": "1-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "1-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 0.02126897744117784,
           "pitch": -0.5303698699906825,
           "rotation": 0,
-          "target": "4-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "4-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 3.0929776222001797,
           "pitch": -0.6386265004394502,
           "rotation": 0,
-          "target": "3-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "3-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 3.0875044682700574,
           "pitch": -0.12435434732858752,
           "rotation": 0,
-          "target": "10-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "10-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 3.13319178298436,
           "pitch": -1.0568117324623536,
           "rotation": 0,
-          "target": "7-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "7-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "9-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "9-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -619,26 +619,26 @@ var APP_DATA = {
           "yaw": 3.1270358478700357,
           "pitch": 0.11886593230616782,
           "rotation": 0,
-          "target": "8-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "8-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.00495589810277508,
           "pitch": 0.15595335618369965,
           "rotation": 0,
-          "target": "11-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "11-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 3.1273501263401595,
           "pitch": -0.13522408394410412,
           "rotation": 0,
-          "target": "10-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "10-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "10-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "10-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -673,26 +673,26 @@ var APP_DATA = {
           "yaw": -3.1226677211060068,
           "pitch": 1.1081918071157766,
           "rotation": 0,
-          "target": "8-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "8-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.010831757430107558,
           "pitch": 0.4809873873626369,
           "rotation": 0,
-          "target": "9-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "9-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": -0.006278687476893197,
           "pitch": 0.2387948177598922,
           "rotation": 0,
-          "target": "11-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "11-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "11-holy-name-of-jesus-roman-catholic-church-harrisburg-pa",
-      "name": "Holy Name of Jesus Roman Catholic Church Harrisburg, PA",
+      "id": "11-holy-name-of-jesus-church-harrisburg-pa",
+      "name": "Holy Name of Jesus Church Harrisburg, PA",
       "levels": [
         {
           "tileSize": 256,
@@ -727,13 +727,13 @@ var APP_DATA = {
           "yaw": 0.004441116143475199,
           "pitch": -0.06872875832478442,
           "rotation": 0,
-          "target": "10-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "10-holy-name-of-jesus-church-harrisburg-pa"
         },
         {
           "yaw": 0.006661643915865767,
           "pitch": 0.17149994993106255,
           "rotation": 0,
-          "target": "9-holy-name-of-jesus-roman-catholic-church-harrisburg-pa"
+          "target": "9-holy-name-of-jesus-church-harrisburg-pa"
         }
       ],
       "infoHotspots": []
